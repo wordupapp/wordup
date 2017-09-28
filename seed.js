717ebe2afa60b08db6febc2cac99548a7372f342
clicksun <clicksun@gmail.com>
2017-09-28 15:18:04 -0500

    Seed file can start graph db server and add users

--- seed.js
+++ seed.js
@@ -2,12 +2,13 @@ const Promise = require('bluebird');
 const chance = require('chance')(123);
 const toonavatar = require('cartoon-avatar');
 
-const { db } = require('./server/db'); // postgres db for users (auth)
-// const graphDb = require('./server/graphDb'); // graph db for all other data
+const { db, graphDb } = require('./server/db');
+
+const session = graphDb.session();
 
 const { User } = require('./server/db/models/');
 
-/* -----------  Set up User data ----------- */
+/* -----------  Set up User data for Postgres ----------- */
 
 const numUsers = 5;
 const userEmails = chance.unique(chance.email, numUsers);
@@ -17,12 +18,13 @@ const adminUser = () => {
   return User.create({
     email: 'admin@admin.admin',
     password: 'admin',
+    name: 'Admin Admin',
     phone: '222-222-2222',
     gender: 'female',
     image: toonavatar.generate_avatar({ gender: 'female' }),
     level: 1,
   })
-    .catch(err => console.error(err));
+    .catch(console.error);
 }
 
 const randomUser = () => {
@@ -32,12 +34,13 @@ const randomUser = () => {
   return User.create({
     email: userEmails.pop(),
     password: chance.string(),
+    name: chance.name({ gender }),
     phone: userPhones.pop(),
     gender,
     image: toonavatar.generate_avatar({ gender }),
     level,
   })
-    .catch(err => console.error(err));
+    .catch(console.error);
 };
 
 const createUsers = () => {
@@ -52,21 +55,45 @@ const seedDb = () => (
   createUsers()
 );
 
-// const seedGrapDb = () => (
 
-// );
+/* -----------  Set up User data for Neo4j ----------- */
 
+const createGraphUsers = pgUsers => {
+  let cypherCode = '';
+  pgUsers.forEach(pgUser => {
+    const { id, name, email, phone, gender, image, level } = pgUser;
+    cypherCode += `CREATE (user${id}:User {
+      pgId:${id},
+      name:'${name}',
+      email:'${email}',
+      phone:'${phone}',
+      gender:'${gender}',
+      image:'${image}',
+      level:${level}
+    })`;
+  });
 
-console.log('Syncing database wordup ...');
+  return session.run(cypherCode);
+};
+
+const seedGrapDb = pgUsers => {
+  const cyperCode = 'MATCH (n) DETACH DELETE n';
+  return session.run(cyperCode)
+    .then(() => createGraphUsers(pgUsers));
+};
+
+/* -----------  Sync databases ----------- */
+
+console.log('Syncing Postgres database wordup ...');
 
 db.sync({ force: true })
   .then(() => {
-    console.log('Seeding database wordup ...');
+    console.log('Seeding Postgres database wordup ...');
     return seedDb();
   })
-  .then((users) => {
-    console.log('users!!!!!!!', users)
-    // return seedGrapDb(users);
+  .then(pgUsers => {
+    console.log('Seeding Neo4j database wordup ...');
+    return seedGrapDb(pgUsers);
   })
   .then(() => {
     console.log('Seeding successful!');
@@ -74,5 +101,6 @@ db.sync({ force: true })
   .catch(console.error)
   .then(() => {
     db.close();
+    session.close();
     return null;
   });
