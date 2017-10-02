0641df5c47a14c162fe3dd630230e6b0fd7b0c69
jannncodes <janineagarcia@gmail.com>
2017-10-02 15:20:29 -0500

    basic functionality and styling before connecting to redux store

--- client/components/Record.jsx
+++ client/components/Record.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Icon } from 'semantic-ui-react'
+import { Icon, Button } from 'semantic-ui-react';
+
 
 const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
@@ -14,9 +15,11 @@ class Record extends Component {
       recording: false,
       results: [],
       userEnded: false,
+      prompt: '',
     };
     this.startRecording = this.startRecording.bind(this);
     this.stopRecording = this.stopRecording.bind(this);
+    this.randomPrompt = this.randomPrompt.bind(this);
   }
 
   componentWillMount() {
@@ -67,52 +70,95 @@ class Record extends Component {
     });
   }
 
-  render() {
-    console.log('here')
-    // const styles = {
+  randomPrompt() {
+    const randomIndex = Math.floor(Math.random() * this.props.prompts.length);
+    this.setState({
+      prompt: this.props.prompts[randomIndex],
+    });
+  }
 
-    // }
+  render() {
+    const styles = {
+      mic: {
+        width: 200,
+        height: 200,
+      },
+      iconOn: {
+        width: 200,
+        height: 200,
+        color: "#0a00b6",
+      },
+      iconOff: {
+        width: 200,
+        height: 200,
+        color: "#d50000",
+      },
+      flex: {
+        width: "100%",
+        height: "100%",
+      },
+      button: {
+        backgroundColor: "#0a00b6",
+        color: "#ffffff",
+      },
+    };
 
     const startRecordingButton = (
       <Icon.Group
         size="huge"
-        color="#0a00b6"
-        onClick={this.startRecording}>
-        <Icon size="big" name="thin-circle" />
-        <Icon name="microphone" />
+        onClick={this.startRecording}
+        style={styles.mic}>
+        <Icon size="big" name="thin circle" style={styles.iconOn} />
+        <Icon name="microphone" style={styles.iconOn} />
       </Icon.Group>
     );
 
     const stopRecordingButton = (
       <Icon.Group
         size="huge"
-        onClick={this.stopRecording}>
-        <Icon size="big" name="thin-circle" />
-        <Icon name="microphone-slash" />
+        onClick={this.stopRecording}
+        style={styles.mic}>
+        <Icon size="big" name="thin circle" style={styles.iconOff} />
+        <Icon name="microphone" style={styles.iconOff} />
       </Icon.Group>
     );
 
     return (
-      <div>
+      <div style={styles.flex}>
+        <div>
+          <h2>Hi, user!</h2>
+        </div>
         {
           !this.state.recording
             ? startRecordingButton
             : stopRecordingButton
         }
+        <div>
+          <h3>Need some prompts?</h3>
+          <Button
+            onClick={this.randomPrompt}
+            style={styles.button}>
+            New prompt
+          </Button>
+          <p>{this.state.prompt}</p>
+        </div>
       </div>
     );
   }
 }
 
-// const mapState = (state, ownProps) => ({
-
-// });
+const mapState = (state, ownProps) => ({
+  prompts: [
+    "If you were a city, which city would you choose to be and why?",
+    "Share a description of your favorite material object that you already own",
+    "Share a best-loved childhood memory",
+    "Who is the most inspirational person to you?",
+  ],
+});
 
 // const mapDispatch = dispatch => ({
 
 // });
-
-const mapState = null;
 
 const mapDispatch = null;
 
--- client/components/index.js
+++ client/components/index.js
@@ -6,4 +6,4 @@
 export { default as Main } from './main';
 export { default as UserHome } from './user-home';
 export { Login, Signup } from './auth-form';
-export { Record } from './Record.jsx';
+export { default as Record } from './Record.jsx';
