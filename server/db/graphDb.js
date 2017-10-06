c94ea0a4f2c3967865b0154ddb1eae2262183d68
Jonathan Kammo <jonathankammo@gmail.com>
2017-10-06 15:22:59 -0500

    Make heroku grapheneDB capable by changing node env variables to production

--- server/db/graphDb.js
+++ server/db/graphDb.js
@@ -1,10 +1,17 @@
 if (process.env.NODE_ENV !== 'production') require('../../secrets');
 const neo4j = require('neo4j-driver').v1;
 
-const graphDb = neo4j.driver(
-  process.env.NEO4j_BOLT_URL,
-  neo4j.auth.basic(process.env.NEO4j_BOLT_USER, process.env.NEO4j_BOLT_PASSWORD)
-);
+if (process.env.NODE_ENV === 'production') {
+  const graphDb = neo4j.driver(
+    process.env.GRAPHENEDB_BOLT_URL,
+    neo4j.auth.basic(process.env.GRAPHENEDB_BOLT_USER, process.env.GRAPHENEDB_BOLT_PASSWORD)
+  );
+} else {
+  const graphDb = neo4j.driver(
+    process.env.NEO4j_BOLT_URL,
+    neo4j.auth.basic(process.env.NEO4j_BOLT_USER, process.env.NEO4j_BOLT_PASSWORD)
+  );
+}
 
 process.on('exit', () => {
   graphDb.close();
