40987419d959c61f658b28c6519764efc7b4d5a6
jannncodes <janineagarcia@gmail.com>
2017-10-09 15:00:55 -0500

    extra animations for user feedback

--- client/components/SynonymGame.jsx
+++ client/components/SynonymGame.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-string-refs */
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'semantic-ui-react';
@@ -18,7 +20,6 @@ class SynonymGame extends Component {
     };
     this.startGame = this.startGame.bind(this);
     this.updateStateAfterClick = this.updateStateAfterClick.bind(this);
-    this.randomPosition = this.randomPosition.bind(this);
     this.nextQuestion = this.nextQuestion.bind(this);
     this.generateWords = this.generateWords.bind(this);
     this.animateWordAfterClick = this.animateWordAfterClick.bind(this);
@@ -26,6 +27,8 @@ class SynonymGame extends Component {
     this.calculateMainWordPosition = this.calculateMainWordPosition.bind(this);
     this.applyAnimationsToWords = this.applyAnimationsToWords.bind(this);
     this.animationUpdate = this.animationUpdate.bind(this);
+    this.animateMainWordifCorrect = this.animateMainWordifCorrect.bind(this);
+    this.animateMainWordifIncorrect = this.animateMainWordifIncorrect.bind(this);
   }
 
   componentWillMount() {
@@ -34,8 +37,11 @@ class SynonymGame extends Component {
     }
   }
 
-  randomPosition(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  calculateMainWordPosition() {
+    return {
+      x: (window.innerWidth / 2) - ((this.state.currentWord.length / 2) * 32),
+      y: (window.innerHeight * 0.7) / 2.5,
+    };
   }
 
   startGame() {
@@ -47,83 +53,57 @@ class SynonymGame extends Component {
     this.setState({
       gameWords: wordsForLevel,
     });
-    window.setTimeout(this.applyAnimationsToWords, 100);
+    window.setTimeout(this.applyAnimationsToWords, 0);
   }
 
   applyAnimationsToWords() {
     this.state.gameWords.forEach(word => {
       const randomDelay = _.random(0, 10, true);
-      const randomDelayMove = _.random(0, 5, true);
       const currentWord = this.refs[word.word];
       const tl = new TimelineMax({ repeat: -1 });
       const tl2 = new TimelineMax({ repeat: -1 });
       tl
-        .to(currentWord, 5, {
+        .to(currentWord, 14, {
           opacity: 1,
           delay: randomDelay,
         })
         .to(currentWord, 3, {
           opacity: 1,
         })
-        .to(currentWord, 5, {
+        .to(currentWord, 14, {
           opacity: 0,
         })
-        .to(currentWord, 5, {
+        .to(currentWord, 4, {
           opacity: 0,
         });
-        tl2
-        .to(currentWord, 3, {
-          x: '+= 50',
-          delay: randomDelayMove,
-          ease: Power3.easeInOut,
-        })
-        .to(currentWord, 3, {
-          x: '-=50',
-          ease: Power3.easeInOut,
+      tl2
+        .to(currentWord, 35, {
+          y: `-=${((window.innerHeight * 0.7) - 50)}`,
+          delay: randomDelay,
         });
     });
   }
 
   generateWords() {
     const gameWords = this.props.gameWords;
-    let renderWords = [];
-    for (let word in gameWords) {
+    let incorrectWords = [];
+    for (const word in gameWords) {
       if (word !== this.state.currentWord) {
-        gameWords[word].forEach(word => renderWords.push(word));
+        gameWords[word].forEach(word => incorrectWords.push(word));
       }
     }
-    renderWords = _.sampleSize(renderWords, 10);
+    incorrectWords = _.sampleSize(incorrectWords, 10);
     const currentWord = Object.keys(this.props.gameWords)[this.state.questionNum];
-    renderWords = renderWords.concat(this.props.gameWords[currentWord]);
-    renderWords = renderWords.map(word => {
+    const correctWords = _.sampleSize(this.props.gameWords[currentWord], 10);
+    let wordsToRender = incorrectWords.concat(correctWords);
+    wordsToRender = wordsToRender.map(word => {
       return {
         word,
-        x: this.randomPosition(50, (window.innerWidth * 0.9) - 50),
-        y: this.randomPosition(50, (window.innerHeight * 0.7) - 50),
+        x: _.random(50, (window.innerWidth * 0.9) - 50),
+        y: _.random(200, (window.innerHeight * 0.7) - 50),
       };
     });
-    return renderWords;
-  }
-
-  animateWordAfterClick(event) {
-    const wordNode = event.target;
-    TweenLite.to(wordNode, 0.6, {
-      x: this.calculateMainWordPosition().x + 100,
-      y: this.calculateMainWordPosition().y + 40,
-      ease: Power3.easeIn,
-      onComplete: this.updateStateAfterClick,
-      onCompleteParams: [event],
-      onUpdate: this.drawLayer,
-    });
-  }
-
-  drawLayer() {
-    this.refs.layer.draw();
-  }
-
-  animationUpdate () {
-    this.drawLayer();
-    requestAnimationFrame(this.animationUpdate);
+    return wordsToRender;
   }
 
   updateStateAfterClick(event) {
@@ -132,6 +112,7 @@ class SynonymGame extends Component {
     let newScore;
     if (currentWordSynonyms.indexOf(clickedWord) === -1) {
       newScore = this.state.currentScore - 1;
+      this.animateMainWordifIncorrect();
     } else {
       newScore = this.state.currentScore + 1;
       if (this.state.gameWords.length === 11) {
@@ -139,11 +120,61 @@ class SynonymGame extends Component {
       } else {
         const updatedWords = this.state.gameWords.filter(wordObj => wordObj.word !== clickedWord);
         this.setState({ gameWords: updatedWords });
+        this.animateMainWordifCorrect();
       }
     }
     this.setState({
       currentScore: newScore,
     });
+  }
+
+  animateWordAfterClick(event) {
+    const wordNode = event.target;
+    TweenLite.to(wordNode, 0.8, {
+      x: this.calculateMainWordPosition().x + 100,
+      y: this.calculateMainWordPosition().y + 40,
+      ease: Power3.easeIn,
+      onComplete: this.updateStateAfterClick,
+      onCompleteParams: [event],
+      // onUpdate: this.drawLayer,
+    });
+  }
+
+  animateMainWordifCorrect() {
+    const mainWord = this.refs.mainWord;
+    const tl = new TimelineMax({ repeat: 2 });
+    tl
+      .to(mainWord, 0.1, {
+        y: '+= 10',
+        ease: Power3.easeInOut,
+      })
+      .to(mainWord, 0.1, {
+        y: '-= 10',
+        ease: Power3.easeInOut,
+      });
+  }
+
+  animateMainWordifIncorrect() {
+    const mainWord = this.refs.mainWord;
+    const tl = new TimelineMax({ repeat: 2 });
+    tl
+      .to(mainWord, 0.2, {
+        x: '+= 10',
+        ease: Power3.easeInOut,
+      })
+      .to(mainWord, 0.2, {
+        x: '-= 10',
+        ease: Power3.easeInOut,
+      });
+  }
+
+  drawLayer() {
+    this.refs.layer.draw();
+  }
+
+  animationUpdate() {
+    this.drawLayer();
+    requestAnimationFrame(this.animationUpdate);
   }
 
   nextQuestion() {
@@ -157,13 +188,6 @@ class SynonymGame extends Component {
     this.setState({
       gameWords: wordsForLevel,
     });
-  }
-
-  calculateMainWordPosition() {
-    return {
-      x: (window.innerWidth / 2) - ((this.state.currentWord.length / 2) * 32),
-      y: (window.innerHeight * 0.7) / 2.5,
-    };
   }
 
   render() {
@@ -193,6 +217,7 @@ class SynonymGame extends Component {
 
     const mainWord = (
       <Text
+        ref="mainWord"
         text={this.state.currentWord}
         fontFamily="Fredoka One"
         fontSize={50}
@@ -202,8 +227,8 @@ class SynonymGame extends Component {
     );
 
     const words = this.state.gameWords;
-    console.log('this.state', this.state)
     requestAnimationFrame(this.animationUpdate);
+
     return (
       <div style={styles.div}>
         <div style={styles.title}>
@@ -229,7 +254,7 @@ class SynonymGame extends Component {
                   return (
                     <Text
                       ref={word.word}
-                      key={index}
+                      key={word.word}
                       text={word.word}
                       fontFamily="Roboto"
                       opacity={0}
