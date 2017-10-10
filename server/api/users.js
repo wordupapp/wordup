41e40e87870044047b208d1c9c376ce7f3454106
clicksun <clicksun@gmail.com>
2017-10-09 22:38:00 -0500

    print text as user speak

--- server/api/users.js
+++ server/api/users.js
@@ -389,7 +389,6 @@ router.post('/:id/words', async (req, res, next) => {
   const { speech } = req.body;
 
   const newWords = await rosetteAnalysis(speech);
-  console.log('new words!!!', newWords)
 
   const newWordPromiseArr = newWords.map(async newWord => {
 
