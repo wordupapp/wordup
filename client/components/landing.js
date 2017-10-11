f85c2198f3f72944513961cad713aea4cd07fd5d
Robert Shaw <r.shaw1@yahoo.com>
2017-10-10 20:32:51 -0500

    created user welcome page

--- client/components/landing.js
+++ client/components/landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Button, Icon } from 'semantic-ui-react';
 
 import Splash from './Splash';
 import {
@@ -19,6 +20,11 @@ const containerStyles = {
   padding: "0px 0px 300px 100px",
 };
 
+const subContainerStyles = {
+  display: "flex",
+  marginTop: "2rem",
+};
+
 const headerStyles = {
   h1: {
     color: '#2b282e',
@@ -28,12 +34,21 @@ const headerStyles = {
     color: '#2b282e',
     fontSize: 28,
   },
+  h4: {
+    color: '#2b282e',
+    fontSize: 20,
+    margin: "auto 2rem",
+  },
   button: {
     marginTop: 25,
   },
 };
 
-const Welcome = props => (
+const iconStyles = {
+  color: '#2b282e',
+};
+
+const Visitor = () => (
   <div style={containerStyles}>
     <h1 style={headerStyles.h1}>
       Want to up your vocabulary game?
@@ -52,6 +67,37 @@ const Welcome = props => (
   </div>
 );
 
+const options = [
+  { key: 0, name: 'Games', ref: 'games', icon: 'trophy', text: 'Play some games to learn new words.' },
+  { key: 1, name: 'Record', ref: 'record', icon: 'microphone', text: 'Record a sample audio to analyze your vocab level.' },
+  { key: 2, name: 'Stats', ref: 'data/1', icon: 'line chart', text: 'Checkout some fun facts about the words you\'ve spoken!' },
+];
+
+const User = () => (
+  <div style={containerStyles}>
+    <h1 style={headerStyles.h1}>
+      What would you like to do today?
+    </h1>
+    {
+      options.map(option => {
+        return (
+          <Link to={`/${option.ref}`} key={option.key} style={subContainerStyles}>
+            <Icon size="huge" name={option.icon} style={iconStyles} />
+            <h4 style={headerStyles.h4}>
+              {option.text}
+            </h4>
+          </Link>
+        )
+      })
+    }
+  </div>
+);
+
+const Welcome = ({ user } = props) => {
+  const authenticated = Object.keys(user).length;
+  return authenticated ? <User /> : <Visitor />;
+};
+
 const Landing = props => {
   let component = "";
   switch (props.match.path) {
@@ -61,7 +107,7 @@ const Landing = props => {
     case "/signup":
       component = <Signup />;
       break;
-    default: component = <Welcome />;
+    default: component = <Welcome user={props.user} />;
   }
   return (
     <Splash>
@@ -70,7 +116,14 @@ const Landing = props => {
   );
 };
 
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(Landing);
+// export default withRouter(Landing);
+export default withRouter(connect(mapStateToProps)(Landing));
+
